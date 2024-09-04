5dc28b76f2605886de0ef8a8f3ce296c71e2a8fc
Joshua Miles <61899128+JoshuaMilesSD@users.noreply.github.com>
2024-09-03 22:11:40 -0700

    Update jsx.js

--- jsx.js
+++ jsx.js
@@ -1,5 +1,17 @@
 import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom";
-
-const App = ()=><h1>Hello world</h1>
+const QuoteBox = (props
+<div id="quote-box">
+  <p id="text">{quote.text}</p>
+  <h2 id="author">{quote.author}</h2>
+  <div class="actions">
+    <button id="new-quote" class="button"
+      onClick={handleNewQuote}>
+      New Quote</button>
+    <a href="twitter.com/intent/tweet"
+      id="tweet-quote" target="_blank">Tweet</a>
+  </div>
+  </div>
+};
+const App = ()=>(<div class="main"><QuoteBox /></div>)
 ReactDOM.render(<App />, document.querySelector("#app"))
